7c6023dccbef1cd57e8ae6489a83462d5c72f976
Tatavo <jgustavosilva_1983@hotmail.com>
2019-07-12 20:43:47 -0500

    mi nueva app

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,15 +2,20 @@ class CommentsController < ApplicationController
     def create
         post = Post.find(params[:post_id])
         comment = post.comments.new(comment_params)
-        comment.save
-        redirect_to post_path(post)
+        comment.user_id = current_user.id
+        if post.save
+            redirect_to post_path(post)    
+        else
+            flash[:errors] = post.errors.messages
+            redirect_to root_path
+        end
     end
     
     def destroy 
         p params
     end
 
-    private
+private
 
     def comment_params
         params.require(:comment).permit(:description)
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,6 +16,7 @@ class PostsController < ApplicationController
 
     def create
         post = Post.new(post_params)
+        post.user_id = current_user.id
         if post.save
             redirect_to post_path(post)    
         else
@@ -31,6 +32,7 @@ class PostsController < ApplicationController
 
     def update
         post = Post.find(params[:id])
+        post.user_id = current_user.id
         post.update(post_params)
         redirect_to post_path(post)
     end
